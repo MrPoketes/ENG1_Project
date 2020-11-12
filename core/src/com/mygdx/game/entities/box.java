a4acf4714190266b3bac5dd78d88fc3c6e907048
sanjel0307 <sanjel0307@gmail.com>
2020-11-12 15:03:25 +0000

    creates Blank Box

---
+++ core/src/com/mygdx/game/entities/box.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.entities;
+
+import com.badlogic.ashley.core.Entity;
+import com.mygdx.game.components.ScreenPosition;
+import com.mygdx.game.components.Size;
+import com.mygdx.game.components.Sprite;
+import com.mygdx.game.components.TestComponent;
+
+public class box extends Entity {
+
+
+    public box(Integer x, Integer y, Integer width, Integer length) { 
+        super(); 
+        this.add(new Size(width, length));
+        this.add(new ScreenPosition(x,y, 0f));
+
+    }
+}
