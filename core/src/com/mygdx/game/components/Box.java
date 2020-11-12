62bba3d99ce3f1a5de638da137e5c7ca60fbaf03
sanjel0307 <sanjel0307@gmail.com>
2020-11-12 15:32:07 +0000

    added Colour component

--- core/src/com/mygdx/game/components/Box.java
+++ core/src/com/mygdx/game/components/Box.java
@@ -3,9 +3,12 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.ComponentMapper;
 
 public class Box implements Component {
-    public static final ComponentMapper<Box> Map = ComponentMapper.getFor(box.class);
+    public static final ComponentMapper<Box> Map = ComponentMapper.getFor(Box.class);
 
     public String box;
 
     public Box(String box) {
         this.box = box;
+
+    }
+}
--- core/src/com/mygdx/game/components/Colour.java
+++ core/src/com/mygdx/game/components/Colour.java
@@ -3,9 +3,13 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.gdx.graphics.Color;
 
-public class ScreenPosition implements Component {
+public class Colour implements Component {
     public static final ComponentMapper<Colour> Map = ComponentMapper.getFor(Colour.class);
 
+
+    private String colour;
+
     public Colour(String colour) { 
-        this.colour = Color colour
+        this.colour = colour;
     }
+}
